db687aa39705032928e19f568a4b5b483322a008
Ayesh1998 <ayeshlak1998@gmail.com>
2020-04-04 13:29:04 +0530

    Added the bottom tab navigation

--- navigation/meals-navigator.js
+++ navigation/meals-navigator.js
@@ -1,27 +1,42 @@
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createAppContainer } from "react-navigation";
 import CategoriesScreen from "../screens/categories-screen";
 import MealDetailsScreen from "../screens/meals-details-screen";
 import CategoryMealsScreen from "../screens/category-meals-screen";
 import Colors from "../constants/colors";
+import FavouriteScreen from "../screens/favourite-screen";
 
 const mealsNavigator = createStackNavigator(
   {
     Categories: CategoriesScreen,
     CategoryMeals: {
-      screen: CategoryMealsScreen
+      screen: CategoryMealsScreen,
     },
-    MealDetail: MealDetailsScreen
+    MealDetail: MealDetailsScreen,
   },
   {
     headerLayoutPreset: "center",
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: Colors.primaryColor
+        backgroundColor: Colors.primaryColor,
       },
-      headerTintColor: "white"
-    }
+      headerTintColor: "white",
+    },
   }
 );
 
-export default createAppContainer(mealsNavigator);
+const mealsTabNavigator = createBottomTabNavigator(
+  {
+    Meals: mealsNavigator,
+    Favourites: FavouriteScreen,
+  }
+  // {
+  //   tabBarOptions: {
+  //     // activeBackgroundColor: Colors.accentColor,
+  //     activeTintColor: Colors.accentColor,
+  //   },
+  // }
+);
+
+export default createAppContainer(mealsTabNavigator);
