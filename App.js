cbb108417d38b3c9779e82b669877bd6e32fc2b6
Ayesh1998 <ayeshlak1998@gmail.com>
2020-04-05 12:35:09 +0530

    Implememted store and mainReducer in app.js and wrap whole components using Provider in react-redux

--- App.js
+++ App.js
@@ -4,13 +4,22 @@ import CategoriesScreen from "./screens/categories-screen";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import MealsNavigator from "./navigation/meals-navigator";
+import { createStore, combineReducers } from "redux";
+import mealsReducer from "./store/meals-reducer";
+import { Provider } from "react-redux";
 
 const getFonts = () => {
   Font.loadAsync({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
 };
+
+const mainReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(mainReducer);
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
@@ -21,7 +30,11 @@ export default function App() {
     );
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -29,6 +42,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+  },
 });
